69a143f31f0466183ee9d093b6c38a6ef28e8616
Sofonias Alemayehu <87647912+HKZIYECH@users.noreply.github.com>
2023-03-25 06:51:53 -1200

    Add files via upload

---
+++ GPA.py
@@ -0,0 +1,348 @@
+#Values
+
+A = 4
+Aa = 3.67
+BB = 3.33
+B = 3
+Bb = 2.67
+CC = 2.33
+C = 2
+Cc = 1.67
+DD = 1.67
+D= 1 
+Dd = 0.67
+F = 0
+
+
+
+#Subject List
+
+eng = input("English: ")
+amh = input("Amharic: ")
+math = input("Math: ")
+bio = input("Biology: ")
+che = input("Chemistry: ")
+phy = input("Physics: ")
+his = input("History: ")
+geo = input("Geography: ")
+civ = input("Civics: ")
+ICT = input("ICT: ")
+PE = input("PE: ")
+
+#If Staements
+
+
+#ENGLISH
+
+if eng == 'A':
+        eeng = 1*4
+elif eng == 'Aa':
+        eeng = 1*3.67
+elif eng == 'BB':
+        eeng = 1*3.33
+elif eng == 'B':
+        eeng = 1*3
+elif eng == 'Bb':
+        eeng = 1*2.67
+elif eng == 'CC':
+        eeng = 1*2.33
+elif eng == 'C':
+        eeng = 1*2
+elif eng == 'Cc':
+        eeng = 1*1.67
+elif eng == 'DD':
+        eeng = 1*1.33
+elif eng == 'D':
+        eeng = 1*1
+elif eng == 'Dd':
+        eeng = 1*0.67
+elif eng == 'F':
+        eeng = 1*0
+
+#AMHARIC
+
+if amh == 'A':
+        aamh = 1*4
+elif amh == 'Aa':
+        aamh = 1*3.67
+elif amh == 'BB':
+        aamh = 1*3.33
+elif amh == 'B':
+        aamh = 1*3
+elif amh == 'Bb':
+        aamh = 1*2.67
+elif amh == 'CC':
+        aamh = 1*2.33
+elif amh == 'C':
+        aamh = 1*2
+elif amh == 'Cc':
+        aamh = 1*1.67
+elif amh == 'DD':
+        aamh = 1*1.33
+elif amh == 'D':
+        aamh = 1*1
+elif amh == 'Dd':
+        aamh = 1*0.67
+elif amh == 'F':
+        aamh = 1*0
+
+#MATH
+
+if math == 'A':
+        mmath = 1*4
+elif math == 'Aa':
+        mmath = 1*3.67
+elif math == 'BB':
+        mmath = 1*3.33
+elif math == 'B':
+        mmath = 1*3
+elif math == 'Bb':
+        mmath = 1*2.67
+elif math == 'CC':
+        mmath = 1*2.33
+elif math == 'C':
+        mmath = 1*2
+elif math == 'Cc':
+        mmath = 1*1.67
+elif math == 'DD':
+        mmath = 1*1.33
+elif math == 'D':
+        mmath = 1*1
+elif math == 'Dd':
+        mmath = 1*0.67
+elif math == 'F':
+        mmath = 1*0
+
+#BIOLOGY
+
+if bio == 'A':
+        bbio = 1*4
+elif bio == 'Aa':
+        bbio = 1*3.67
+elif bio == 'BB':
+        bbio = 1*3.33
+elif bio == 'B':
+        bbio = 1*3
+elif bio == 'Bb':
+        bbio = 1*2.67
+elif bio == 'CC':
+        bbio = 1*2.33
+elif bio == 'C':
+        bbio = 1*2
+elif bio == 'Cc':
+        bbio = 1*1.67
+elif bio == 'DD':
+        bbio = 1*1.33
+elif bio == 'D':
+        bbio = 1*1
+elif bio == 'Dd':
+        bbio = 1*0.67
+elif bio == 'F':
+        bbio = 1*0
+
+#CHEMISTRY
+
+if che == 'A':
+        cche = 1*4
+elif che == 'Aa':
+        cche = 1*3.67
+elif che == 'BB':
+        cche = 1*3.33
+elif che == 'B':
+        cche = 1*3
+elif che == 'Bb':
+        cche = 1*2.67
+elif che == 'CC':
+        cche = 1*2.33
+elif che == 'C':
+        cche = 1*2
+elif che == 'Cc':
+        cche = 1*1.67
+elif che == 'DD':
+        cche = 1*1.33
+elif che == 'D':
+        cche = 1*1
+elif che == 'Dd':
+        cche = 1*0.67
+elif che == 'F':
+        cche = 1*0
+
+#PHYSICS
+
+if phy == 'A':
+        pphy = 1*4
+elif phy == 'Aa':
+        pphy = 1*3.67
+elif phy == 'BB':
+        pphy = 1*3.33
+elif phy == 'B':
+        pphy = 1*3
+elif phy == 'Bb':
+        pphy = 1*2.67
+elif phy == 'CC':
+        pphy = 1*2.33
+elif phy == 'C':
+        pphy = 1*2
+elif phy == 'Cc':
+        pphy = 1*1.67
+elif phy == 'DD':
+        pphy = 1*1.33
+elif phy == 'D':
+        pphy = 1*1
+elif phy == 'Dd':
+        pphy = 1*0.67
+elif phy == 'F':
+        pphy = 1*0
+
+#HISTORY
+
+if his == 'A':
+        hhis = 0.75*4
+elif his == 'Aa':
+        hhis = 0.75*3.67
+elif his == 'BB':
+        hhis = 0.75*3.33
+elif his == 'B':
+        hhis = 0.75*3
+elif his == 'Bb':
+        hhis = 0.75*2.67
+elif his == 'CC':
+        hhis = 0.75*2.33
+elif his == 'C':
+        hhis = 0.75*2
+elif his == 'Cc':
+        hhis = 0.75*1.67
+elif his == 'DD':
+        hhis = 0.75*1.33
+elif his == 'D':
+        hhis = 0.75*1
+elif his == 'Dd':
+        hhis = 0.75*0.67
+elif his == 'F':
+        hhis = 0.75*0
+
+#GEOGRAPHY
+
+if geo == 'A':
+        ggeo = 0.5*4
+elif geo == 'Aa':
+        ggeo = 0.5*3.67
+elif geo == 'BB':
+        ggeo = 0.5*3.33
+elif geo == 'B':
+        ggeo = 0.5*3
+elif geo == 'Bb':
+        ggeo = 0.5*2.67
+elif geo == 'CC':
+        ggeo = 0.5*2.33
+elif geo == 'C':
+        ggeo = 0.5*2
+elif geo == 'Cc':
+        ggeo = 0.5*1.67
+elif geo == 'DD':
+        ggeo = 0.5*1.33
+elif geo == 'D':
+        ggeo = 0.5*1
+elif geo == 'Dd':
+        ggeo = 0.5*0.67
+elif geo == 'F':
+        ggeo = 0.5*0
+
+#CIVICS
+
+if civ == 'A':
+        cciv = 0.5*4
+elif civ == 'Aa':
+        cciv = 0.5*3.67
+elif civ == 'BB':
+        cciv = 0.5*3.33
+elif civ == 'B':
+        cciv = 0.5*3
+elif civ == 'Bb':
+        cciv = 0.5*2.67
+elif civ == 'CC':
+        cciv = 0.5*2.33
+elif civ == 'C':
+        cciv = 0.5*2
+elif civ == 'Cc':
+        cciv = 0.5*1.67
+elif civ == 'DD':
+        cciv = 0.5*1.33
+elif civ == 'D':
+        cciv = 0.5*1
+elif civ == 'Dd':
+        cciv = 0.5*0.67
+elif civ == 'F':
+        cciv = 0.5*0
+
+#ICT
+
+if ICT == 'A':
+        IICT = 0.5*4
+elif ICT == 'Aa':
+        IICT = 0.5*3.67
+elif ICT == 'BB':
+        IICT = 0.5*3.33
+elif ICT == 'B':
+        IICT = 0.5*3
+elif ICT == 'Bb':
+        IICT = 0.5*2.67
+elif ICT == 'CC':
+        IICT = 0.5*2.33
+elif ICT == 'C':
+        IICT = 0.5*2
+elif ICT == 'Cc':
+        IICT = 0.5*1.67
+elif ICT == 'DD':
+        IICT = 0.5*1.33
+elif ICT == 'D':
+        IICT = 0.5*1
+elif ICT == 'Dd':
+        IICT = 0.5*0.67
+elif ICT == 'F':
+        IICT = 0.5*0
+
+#PE
+
+if PE == 'A':
+        PEE = 0.5*4
+elif PE == 'Aa':
+        PEE = 0.5*3.67
+elif PE == 'BB':
+        PEE = 0.5*3.33
+elif PE == 'B':
+        PEE = 0.5*3
+elif PE == 'Bb':
+        PEE = 0.5*2.67
+elif PE == 'CC':
+        PEE = 0.5*2.33
+elif PE == 'C':
+        PEE = 0.5*2
+elif PE == 'Cc':
+        PEE = 0.5*1.67
+elif PE == 'DD':
+        PEE = 0.5*1.33
+elif PE == 'D':
+        PEE = 0.5*1
+elif PE == 'Dd':
+        PEE = 0.5*0.67
+elif PE == 'F':
+        PEE = 0.5*0
+
+#ADD
+
+sum = float(eeng) + float(aamh) +  float(mmath) + float(bbio) + float(cche) + float(pphy) + float(hhis) + float(ggeo) + float(cciv) + float(IICT) + float(PEE)  
+
+GPA = sum / 8.75
+
+print("Your GPA is: " + str(GPA) )
+
+#Made by Sofonias Alemayehu 10B
+
+if GPA <3.2:
+        print("Anchi Keleme")
+
+elif GPA <3.2:
+        print("Dedeb")
+
+jj = input("Press any key to exit")
